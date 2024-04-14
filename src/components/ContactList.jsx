2c985f987efc8e7ddf5b3f2eb89eee091497a7d6
KasiaGrabska <143888097+KasiaGrabska@users.noreply.github.com>
2024-04-14 20:46:24 +0200

    add css

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,3 +1,4 @@
+import styles from '../styles.css/contactList.module.css';
 export const ContactList = ({ contacts, deleteContact }) => {
   return (
     <>
@@ -5,7 +6,12 @@ export const ContactList = ({ contacts, deleteContact }) => {
         {contacts.map(contact => (
           <li key={contact.id}>
             {contact.name}: {contact.number}
-            <button onClick={() => deleteContact(contact.id)}>Delete</button>
+            <button
+              onClick={() => deleteContact(contact.id)}
+              className={styles.buttonDelete}
+            >
+              Delete
+            </button>
           </li>
         ))}
       </ul>
